ad9f207207f1fe2d29931b18043b184e4fed2cd2
jarl.ostensen <jarl.ostensen@gmail.com>
2022-12-17 15:13:19 +0000

    Verified Hive is working.

--- tests/test_core.c
+++ tests/test_core.c
@@ -1,13 +1,43 @@
 #include <string.h>
 #include <stdio.h>
+
+#define _JOSRT_IMPLEMENT_ALLOCATORS
+
 #include <extensions/hex_dump.h>
+#include <extensions/arena_allocator.h>
 
 #define _JOS_IMPLEMENT_HIVE
 #define _JOS_IMPLEMENT_CONTAINERS
 #include <extensions/hive.h>
 
-void null_print_hex_line(const char* l) {
+static void null_print_hex_line(const char* l) {
     (void)l;
+}
+
+static void test_hive() {
+    static char a_large_buffer[0x200000];
+    arena_allocator_t* allocator = arena_allocator_create(a_large_buffer, sizeof(a_large_buffer));
+    hive_t hive;
+    hive_create(&hive, (generic_allocator_t*)allocator);
+    hive_set(&hive, "foo", HIVE_VALUE_INT(1001), HIVE_VALUE_STR("bar"), HIVE_VALUELIST_END);
+    hive_set(&hive, "foo", HIVE_VALUE_INT(42), HIVE_VALUE_STR("bar"), HIVE_VALUE_INT(999999), HIVE_VALUELIST_END);
+    // create list1 and set some values
+	hive_lpush(&hive, "list1",
+		HIVE_VALUE_INT(1), HIVE_VALUE_STR("one"),
+		HIVE_VALUE_INT(2), HIVE_VALUE_STR("two"),
+		HIVE_VALUE_INT(3), HIVE_VALUE_STR("three"),
+		HIVE_VALUELIST_END);
+
+	// this will append values to list1
+	hive_lpush(&hive, "list1",
+		HIVE_VALUE_INT(4), HIVE_VALUE_STR("four"),
+		HIVE_VALUE_INT(5), HIVE_VALUE_STR("five"),
+		HIVE_VALUE_INT(6), HIVE_VALUE_STR("six"),
+		HIVE_VALUELIST_END);
+
+	vector_t values;
+	vector_create(&values, 10, sizeof(hive_value_t), (generic_allocator_t*)allocator);
+	hive_lget(&hive, "list1", &values);
 }
 
 int main(int argc, char* argv[]) {
@@ -18,6 +48,7 @@ int main(int argc, char* argv[]) {
     result = snprintf(buffer, sizeof(buffer), "Hello, this is %s and a %d...", "snprintf", result);
 
     hex_dump_mem(null_print_hex_line, buffer, result+8, k8bitInt);
+    test_hive();
 
     return result;
 }
--- tests/test_printf.c
+++ tests/test_printf.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <extensions/string.h>
+
+#define _JOSRT_IMPLEMENT_ALLOCATORS
+
 #include <extensions/hex_dump.h>
+#include <extensions/arena_allocator.h>
+
+#define _JOS_IMPLEMENT_HIVE
+#define _JOS_IMPLEMENT_CONTAINERS
+#include <extensions/hive.h>
 
 extern void _syscall_write(const char* str, int len);
 int sys_write(int fd, const char* data, size_t len) {
@@ -13,8 +21,58 @@ int sys_write(int fd, const char* data, size_t len) {
     return -1;
 }
 
-void print_hex_line(const char* line) {
+static void print_hex_line(const char* line) {
     printf("%s\n", line);
+}
+
+static void test_hive() {
+    static char a_large_buffer[0x200000];
+
+    arena_allocator_t* allocator = arena_allocator_create(a_large_buffer, sizeof(a_large_buffer));
+    hive_t hive;
+    hive_create(&hive, (generic_allocator_t*)allocator);
+    hive_set(&hive, "foo", HIVE_VALUE_INT(1001), HIVE_VALUE_STR("bar"), HIVE_VALUELIST_END);
+    hive_set(&hive, "foo", HIVE_VALUE_INT(42), HIVE_VALUE_STR("bar"), HIVE_VALUE_INT(999999), HIVE_VALUELIST_END);
+    // create list1 and set some values
+	hive_lpush(&hive, "list1",
+		HIVE_VALUE_INT(1), HIVE_VALUE_STR("one"),
+		HIVE_VALUE_INT(2), HIVE_VALUE_STR("two"),
+		HIVE_VALUE_INT(3), HIVE_VALUE_STR("three"),
+		HIVE_VALUELIST_END);
+
+	// this will append values to list1
+	hive_lpush(&hive, "list1",
+		HIVE_VALUE_INT(4), HIVE_VALUE_STR("four"),
+		HIVE_VALUE_INT(5), HIVE_VALUE_STR("five"),
+		HIVE_VALUE_INT(6), HIVE_VALUE_STR("six"),
+		HIVE_VALUELIST_END);
+
+	vector_t values;
+	vector_create(&values, 10, sizeof(hive_value_t), (generic_allocator_t*)allocator);
+	hive_lget(&hive, "list1", &values);
+
+    // this prints
+	//  "1 one 2 two 3 three 4 four 5 five 6 six"
+	
+	const size_t num_elements = vector_size(&values);
+	for (size_t n = 0; n < num_elements; ++n) {
+		hive_value_t* hive_value = (hive_value_t*)vector_at(&values, n);
+		hive_value_type_t type = hive_value->type;
+		switch (type) {
+		case kHiveValue_Int:
+		{
+			printf("%lld ", hive_value->value.as_int);
+		}
+		break;
+		case kHiveValue_Str:
+		{
+			printf("%s ", hive_value->value.as_str);
+		}
+		break;
+		default:;
+		}
+	}
+    printf("\n");
 }
 
 int main(int argc, char* argv[], char* envp[]) {    
@@ -27,6 +85,7 @@ int main(int argc, char* argv[], char* envp[]) {
     printf("The result is \"%s\" returned %d\n", buffer, result);
 
     hex_dump_mem(print_hex_line, buffer, result+8, k8bitInt);
+    test_hive();
 
     return 0;
 }
