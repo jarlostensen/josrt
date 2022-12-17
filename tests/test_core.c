77edb74b932d1ed085ebcecec3f71ceea33bea25
jarl.ostensen <jarl.ostensen@gmail.com>
2022-12-17 10:51:50 +0000

    added null hex_dump test

--- tests/test_core.c
+++ tests/test_core.c
@@ -1,5 +1,10 @@
 #include <string.h>
 #include <stdio.h>
+#include <extensions/hex_dump.h>
+
+void null_print_hex_line(const char* l) {
+    (void)l;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -7,5 +12,8 @@ int main(int argc, char* argv[]) {
     int result = strlen(message);    
     char buffer[1024];
     result = snprintf(buffer, sizeof(buffer), "Hello, this is %s and a %d...", "snprintf", result);
+
+    hex_dump_mem(null_print_hex_line, buffer, result+8, k8bitInt);
+
     return result;
 }
