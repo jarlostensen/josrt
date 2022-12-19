1a931783e00cbababdfb9a6bfcbef00d39bc0762
jarl.ostensen <jarl.ostensen@gmail.com>
2022-12-19 13:30:25 +0000

    Added some boiler plate to libc_main

--- src/crt/libc_main.c
+++ src/crt/libc_main.c
@@ -14,10 +14,12 @@ uint32_t xcr0;
 	return ((xcr0 & 6) == 6); /* checking if xmm and ymm state are enabled in XCR0 */
 }
 
+#ifdef _JOSRT_HOSTED_ELF
+// *nux ELF __libc_start_main 
 // see https://hammertux.github.io/libc-start 
 void __libc_start_main(int *(main) (int, char * *),
 					  int argc,
-					  char * * ubp_av,
+					  char** argv,
 					  void (*init) (void),
 					  void (*fini) (void),
 					  void (*rtld_fini) (void),
@@ -25,6 +27,21 @@ void __libc_start_main(int *(main) (int, char * *),
 
 	int exit_code = -1;
 
+	// using MUSL's implementation
+
+	char **envp = argv+argc+1;
+	int i;
+	for (i=0; envp[i]; i++) ;
+
+#define AUX_CNT 38
+
+	size_t aux[AUX_CNT] = {0};
+	size_t* auxv = (size_t*)(envp+i+1);
+	for (i=0; auxv[i]; i+=2) if (auxv[i]<AUX_CNT) aux[auxv[i]] = auxv[i+1];
+	char* pn;
+	if (!pn) pn = (char*)aux[31];
+	if (!pn) pn = "";
+	
 	// check if XMM/YMM state is enabled and AVX2 support is present
 	if ( check_xcr0_ymm() ) {
 		//TODO: check leaf 7 support
@@ -33,18 +50,18 @@ void __libc_start_main(int *(main) (int, char * *),
 		ecx = 0;
 		__cpuid(0x7, eax, ebx, edx, ecx);
 		if ( ebx & bit_AVX2 ) {
-			//TODO: proper argc + argv
-			main(1,0);
+			main(argc,argv);
 			exit_code = 0;
 		}
 		else {
-			printf("[__libc_start_main] unsupported HW; AVX2 not enabled\n");
+			//printf("[__libc_start_main] unsupported HW; AVX2 not enabled\n");
 			exit_code = -2;
 		}
 	}
 	else {
-		printf("[__libc_start_main] unsupported HW; XMM/YMM not enabled\n");
+		//printf("[__libc_start_main] unsupported HW; XMM/YMM not enabled\n");
 		exit_code = -3;
 	}
     __exit(exit_code);
 }
+#endif // _JOSRT_HOSTED_ELF
